5b5f2ca21736e1400d5749e7f0625d2ffff06f0d
monte-dev <badziobw@gmail.com>
2023-09-30 13:08:42 +0100

    add(backend feat): crud methods for user management in usersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -13,4 +13,98 @@ export class UsersService {
   public async getAll(): Promise<User[]> {
     return this.prismaService.user.findMany();
   }
+
+  public async getById(id: User['id']): Promise<User | null> {
+    const user = await this.prismaService.user.findUnique({
+      where: { id },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found...');
+    }
+
+    return user;
+  }
+
+  public async getByEmail(
+    email: User['email'],
+  ): Promise<(User & { password: Password }) | null> {
+    const user = await this.prismaService.user.findUnique({
+      where: { email },
+      include: { password: true },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found...');
+    }
+
+    return user;
+  }
+
+  public async createUser(
+    userData: Omit<User, 'id' | 'role'>,
+    password: Password['hashedPassword'],
+  ) {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...userData,
+          password: {
+            create: {
+              hashedPassword: password,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2002' && error.meta.target.includes('email')) {
+        throw new ConflictException('User with this email already exists');
+      }
+      throw error;
+    }
+  }
+
+  public async updateById(
+    userId: User['id'],
+    userData: Omit<User, 'id' | 'role'>,
+    password?: string | undefined,
+  ) {
+    if (password !== undefined) {
+      return await this.prismaService.user.update({
+        where: { id: userId },
+        data: {
+          ...userData,
+          password: {
+            update: {
+              hashedPassword: password,
+            },
+          },
+        },
+      });
+    }
+
+    try {
+      return await this.prismaService.user.update({
+        where: { id: userId },
+        data: userData,
+      });
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException(`User ID: ${userId} not found.`);
+      }
+      throw error;
+    }
+  }
+
+  async deleteById(id: User['id']) {
+    try {
+      return this.prismaService.user.delete({
+        where: { id },
+      });
+    } catch (error) {
+      if (error.code === 'P2025') {
+      }
+      throw new NotFoundException(`User ID: ${id} not found.`);
+    }
+  }
 }
