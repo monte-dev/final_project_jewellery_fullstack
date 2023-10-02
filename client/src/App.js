173e778d3061811dc2150c9df6d2f535d75554d7
monte-dev <badziobw@gmail.com>
2023-10-02 23:57:25 +0100

    Add(frontend): client side routing and redux store setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/pages/Home/Home.jsx';
+import NotFound from './components/pages/NotFound/NotFound.jsx';
+import Cart from './components/pages/Cart/Cart.jsx';
+import Order from './components/pages/Order/Order.jsx';
+import Product from './components/pages/Product/Product.jsx';
+import Login from './components/pages/Login/Login.jsx';
+import Register from './components/pages/Register/Register.jsx';
 
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/product/:id" element={<Product />}></Route>
+        <Route path="/order" element={<Order />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="*" element={<NotFound />}></Route>
+      </Routes>
+    </div>
+  );
+};
 export default App;
---
+++ client/src/redux/store.js
@@ -0,0 +1,25 @@
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import initialState from './initialState';
+import productsReducer from './productsReducer.js';
+import usersReducer from './usersReducer.js';
+
+const subreducers = {
+  products: productsReducer,
+  users: usersReducer,
+};
+
+const rootReducer = combineReducers(subreducers);
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f,
+  ),
+);
+
+export default store;
