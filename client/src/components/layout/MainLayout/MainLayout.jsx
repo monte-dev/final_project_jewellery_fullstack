ea2970cc4920e07eb840460715786560a3343f61
monte-dev <badziobw@gmail.com>
2023-10-07 19:47:18 +0100

    Add(frontend product): Added styling to Product component, modify styling in MainLayout

--- client/src/components/layout/MainLayout/MainLayout.jsx
+++ client/src/components/layout/MainLayout/MainLayout.jsx
@@ -1,15 +1,16 @@
 import NavBar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
+import styles from './MainLayout.module.css';
 
 const MainLayout = ({ children }) => {
   return (
-    <div>
+    <>
       <NavBar />
       <Header />
-      {children}
+      <div className={styles.container}>{children}</div>
       <Footer />
-    </div>
+    </>
   );
 };
 export default MainLayout;
--- client/src/components/pages/Product/Product.jsx
+++ client/src/components/pages/Product/Product.jsx
@@ -1,19 +1,24 @@
 import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { fetchSingleProductFromAPI } from '../../../redux/productsReducer.js';
+import { useNavigate, useParams } from 'react-router-dom';
+import {
+  fetchSingleProductFromAPI,
+  getProductById,
+} from '../../../redux/productsReducer.js';
 import { Badge, Button, Container, Row } from 'react-bootstrap';
 import { HiShoppingBag, HiStar } from 'react-icons/hi';
 
 import styles from './product.module.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { HiArrowLeft } from 'react-icons/hi';
 
 const Product = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
-  const currentProduct = useSelector((state) => state.products.singleProduct);
+  const navigate = useNavigate();
+  const currentProduct = useSelector((state) => getProductById(state, id));
+  const [quantity, setQuantity] = useState(1);
 
-  console.log(currentProduct);
   useEffect(() => {
     if (!currentProduct) {
       dispatch(fetchSingleProductFromAPI(id));
@@ -21,41 +26,72 @@ const Product = () => {
   }, [dispatch, id, currentProduct]);
   // console.log(currentProduct.images[1].name);
 
+  const handleAddToCart = () => {};
+
   return (
-    <div>
+    <Container className={styles.product}>
+      <Button
+        variant="outline-dark"
+        className="my-2 py-1"
+        onClick={() => navigate('/')}
+      >
+        <HiArrowLeft className="mb-1 me-2" size={18} />
+        <span className="fs-6">Return</span>
+      </Button>
       {currentProduct ? (
-        <Container className="p-5">
-          <Row>
-            <img alt={currentProduct.name} src={currentProduct.images}></img>
-            <h3>{currentProduct.name}</h3>
-            <ul className={styles.productList}>
-              <li>{`Price: $${currentProduct.price}`}</li>
-              <li>
-                <span>Rating:</span>
-                <HiStar />
-                <span> {currentProduct.rating}</span>
-              </li>
-              <li>
-                {currentProduct.stockQuantity > 5 ? (
-                  <Badge bg="success">Stock Available</Badge>
-                ) : currentProduct.stockQuantity > 0 ? (
-                  <Badge bg="warning">Selling Fast</Badge>
-                ) : (
-                  <Badge bg="danger">Out of Stock</Badge>
-                )}
-              </li>
-            </ul>
-            <p>{currentProduct.description}</p>
-          </Row>
-          <Button>
-            <span className="p-2">Add To Cart</span>
-            <HiShoppingBag className="mb-1 ms-2" />
-          </Button>
-        </Container>
+        <Row>
+          <div className={styles.productWrapper}>
+            <aside className={styles.productImagesWrapper}>
+              <img
+                className={styles.productImage}
+                fluid
+                alt={currentProduct.name}
+                // src={currentProduct.images}
+                src="/assets/images/products/placeholder.svg"
+              ></img>
+            </aside>
+            <section className={styles.productInfo}>
+              <h3>{currentProduct.name}</h3>
+              <dl className={styles.productDescription}>
+                <dd>
+                  {currentProduct.stockQuantity > 5 ? (
+                    <Badge bg="success">Stock Available</Badge>
+                  ) : currentProduct.stockQuantity > 0 ? (
+                    <Badge bg="warning">Selling Fast</Badge>
+                  ) : (
+                    <Badge bg="danger">Out of Stock</Badge>
+                  )}
+                </dd>
+                <dd className="fs-3">{`$${currentProduct.price}`}</dd>
+                <dd>
+                  <span>Rating:</span>
+                  <HiStar />
+                  <span> {currentProduct.rating}</span>
+                </dd>
+              </dl>
+              <p>{currentProduct.description}</p>
+              <div className={styles.productAction}>
+                <dt className={styles.productActionItem}>Quantity:</dt>
+                <dd className={styles.productActionItem}>
+                  <input
+                    type="number"
+                    min="1"
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)}
+                  />
+                </dd>
+                <Button onClick={handleAddToCart}>
+                  <span>Add To Cart</span>
+                  <HiShoppingBag className="mb-1 ms-2" />
+                </Button>
+              </div>
+            </section>
+          </div>
+        </Row>
       ) : (
         <p>Loading...</p>
       )}
-    </div>
+    </Container>
   );
 };
 export default Product;
